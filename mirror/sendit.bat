7ba5590db03950a64ceeba655399bf039d4effbe
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-15 09:55:17 +0700

    Update sendit.bat

--- mirror/sendit.bat
+++ mirror/sendit.bat
@@ -1,11 +1,3 @@
 @echo off
-echo Memulai pengiriman file besar via SFTP...
 
-(
-  echo binary
-  echo progress
-  echo put "C:\wadah\hasil.zip" ~/
-  echo bye
-) | sftp -P 22 -o StrictHostKeyChecking=no fool@192.168.1.122
-
-echo Pengiriman selesai!
+scp -o StrictHostKeyChecking=no -P 22 "C:\wadah\hasil.zip" fool@192.168.1.41:~/
