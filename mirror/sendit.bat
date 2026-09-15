18cde38316c4cd20663b7dd93715d9497909c65a
dreadnought24-rgb <sofyanrezky097@gmail.com>
2026-09-15 09:45:31 +0700

    Change SFTP target IP address in sendit.bat

--- mirror/sendit.bat
+++ mirror/sendit.bat
@@ -6,6 +6,6 @@ echo Memulai pengiriman file besar via SFTP...
   echo progress
   echo put "C:\wadah\hasil.zip" ~/
   echo bye
-) | sftp -P 22 -o StrictHostKeyChecking=no fool@192.168.1.41
+) | sftp -P 22 -o StrictHostKeyChecking=no fool@192.168.1.122
 
 echo Pengiriman selesai!
